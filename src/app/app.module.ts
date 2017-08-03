c6ca608ab0ecc9b443d93b8f6156b06e4cbe38e1
Pedro Esperança <buesimples@gmail.com>
2017-08-03 12:16:11 +0100

    3. Create Pages Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    HomeComponent,
+    TodoListComponent
   ],
   imports: [
     BrowserModule
